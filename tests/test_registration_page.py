0f83ba3586c0153aaad025da48ffe210de0a7be5
masha <masha-krim@yandex.ru>
2024-06-23 23:55:40 +0500

    allure added

---
+++ tests/test_registration_page.py
@@ -0,0 +1,13 @@
+from hw_11.data.user import User
+from hw_11.pages.registration_page import RegistrationFormPage
+
+
+def test_student_registration_form():
+    registration_page = RegistrationFormPage()
+    masha = User(first_name='Masha', last_name='Www', email='test@gmail.com', gender='Female',
+                 phone='1234456789', date_of_birth_day='11', date_of_birth_month='May', date_of_birth_year='1999',
+                 subject='Maths', hobby='Sports', picture='picture.jpg',
+                 address='Quitzon Common, South Kraigville', state='NCR', city='Delhi')
+    registration_page.open()
+    registration_page.register(masha)
+    registration_page.should_have_registered(masha)
