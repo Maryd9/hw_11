b25fd32f6fd4fece5a2f9425b07a726b9e14e786
masha <masha-krim@yandex.ru>
2024-06-24 00:31:27 +0500

    added fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
-from selene import browser
+from selene import browser, Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -9,5 +11,29 @@ def open_browser():
     browser.config.window_height = 768
 
     yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "122.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+
+    yield browser
 
     browser.quit()
